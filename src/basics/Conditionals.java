fee3b564827e1cace1900ed3bd99b1505733b8fa
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-07-18 21:20:49 +0530

    Conditionals

---
+++ src/basics/Conditionals.java
@@ -0,0 +1,42 @@
+package basics;
+
+public class Conditionals {
+    public static void main(String [] args){
+
+
+//        if(i>5 && i<10){
+//            System.out.println("i is 5");
+//        } else{
+//            System.out.println("i is not 5");
+//        }
+        if(i%2==0){
+            System.out.println("i is even");
+        } else{
+            System.out.println("i is odd");
+        }
+
+        //nested if
+//        if(i>5){
+//            if(i<10){
+//
+//            }else{
+//
+//            }
+//        }else{
+//
+//        }
+
+
+        boolean b=true;
+        int i=6;
+//        //if else if ladder
+//        if(i==5){
+//            //execute this
+//        } else if(i==6){
+//            //execute this
+//        } else{
+//            //execute
+//        }
+
+    }
+}
---
+++ src/basics/Variables.java
@@ -0,0 +1,7 @@
+package basics;
+
+public class Variables {
+    public static void main(String[] args){
+        //integers
+    }
+}
