9204b0f5f12f1984b48f7ee3f4f16bc8488c07b9
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-07-19 15:52:18 +0530

    Loops

---
+++ src/basics/Loops.java
@@ -0,0 +1,39 @@
+package basics;
+
+public class Loops {
+    public static void main(String [] args) {
+        //loops-> while loop
+        //int i=1;
+//        while(i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while(i<=10);
+
+
+        //1 to 10
+//        for (int i = 0; i <=10; i++) {
+//            System.out.println(i);
+//        }
+        /*
+        flow of control of for loop
+        1.First assignement statement is executed(Variable Defination)
+        2.Then second conditional statement iis evaluated(T/F)
+        3.if true->control flow will evaluate the body of the loop
+        4.Once loop is finished,control flow will go back to the for statement, and third increment statement will be evaluated
+        5.Again,conditional statement is evaluated
+        6.Repeat  2-5
+         */
+
+        //nested loops
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+
+            }
+        }
+    }
+    }
+
