5c8552336231841c3df42ec13040069d8efd5449
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-07-31 16:42:43 +0530

    String Basics

---
+++ src/Array/Strings.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Strings {
+    public static void main(String[] args){
+        String fname="Mehul";
+        String lname="Singh";
+
+        System.out.println(fname+" "+lname);
+    }
+}
