5192bc990a711aa2b4b2c0aee16d73f04c7253fe
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-08-04 01:15:41 +0530

    Classes And Objects

---
+++ src/oops/ClassandObjects.java
@@ -0,0 +1,42 @@
+package oops;
+
+public class ClassandObjects {
+    public static void main(String[] args){
+        Student s1=new Student();
+        Student s2=new Student();
+
+
+        s1.name="Aditya";
+        s1.age=28;
+        s1.rollNumber=100;
+        s1.college="IIT DHOLAKPUR";
+
+        s2.name="Rohit";
+        s2.age=80;
+        s2.rollNumber=101;
+        s2.college="IIT CHINA";
+
+        s1.markAttendance();
+        s2.markAttendance();
+
+        s1.print();
+        s2.print();
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    int rollNumber;
+    String college;
+
+    void markAttendance(){
+        System.out.println("Attendance Marked by"+name);
+    }
+
+    void print(){
+        System.out.println(name+" "+age+" "+college);
+    }
+}
+
+//Java is almost complete OOP langauge.
