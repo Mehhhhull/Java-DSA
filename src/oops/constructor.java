bbc33951a51a140e6cd6d71c05adbe4d8d38b3be
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-08-05 00:46:38 +0530

    Constructor

---
+++ src/oops/constructor.java
@@ -0,0 +1,53 @@
+package oops;
+
+public class constructor {
+    public static void main(String[] args) {
+
+
+        Student s1 = new Student("Rohit",28,102,"IIT G");
+
+
+        //default values
+//        System.out.println(s1.name);
+//        System.out.println(s1.age);
+//        System.out.println(s1.rollNumber);
+//        System.out.println(s1.college);
+
+        int x=4;//local variable- no default value
+    }
+}
+/*
+int-0
+float->0.0
+bool->null
+str->null(nothing)
+ */
+
+class student{
+    String name;//instance variables-they have default values
+    int age;
+    int rollNumber;
+    String college;
+
+    //instance methods-func inside class
+    void markAttendance(){
+        System.out.println("Attendance Marked by"+name);
+    }
+
+    //default constructor
+    student(){
+    }
+
+    student(String n,int a,int rn,String c){
+        name=n;
+        age=a;
+        rollNumber=rn;
+        college=c;
+    }
+
+
+    void print(){
+        System.out.println(name+" "+age+" "+college);
+    }
+}
+
